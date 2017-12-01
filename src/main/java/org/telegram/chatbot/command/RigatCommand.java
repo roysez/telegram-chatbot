ca69e51f15f3611cc4b614ca65f58f4b063ec4c3
roysez <sergiobaluh@gmail.com>
2017-12-02 01:43:30 +0200

    test

--- src/main/java/org/telegram/chatbot/command/RigatCommand.java
+++ src/main/java/org/telegram/chatbot/command/RigatCommand.java
@@ -2,6 +2,7 @@ package org.telegram.chatbot.command;
 
 import org.telegram.chatbot.utils.Dictionary;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
@@ -13,8 +14,14 @@ public class RigatCommand extends BotCommand {
 
     @Override
     public void execute(TelegramLongPollingBot bot, Update update) throws TelegramApiException {
+
+        DeleteMessage deleteMessage = new DeleteMessage(update.getMessage().getChatId(),update.getMessage().getMessageId());
+        deleteMessage.validate();
+
+
         bot.execute(new SendMessage()
                 .setChatId(update.getMessage().getChatId())
                 .setText(Dictionary.randomPhrase()));
+        bot.execute(deleteMessage);
     }
 }
